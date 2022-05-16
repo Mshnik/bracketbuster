8bb5b3d5d35b33a7321874d1ef734a1b73f9d49b
Mshnik <Mgpshnik@gmail.com>
2022-05-16 08:20:00 -0400

    Add test of full calculation method.

--- src/test/java/com/redpup/bracketbuster/sim/CalculationsTest.java
+++ src/test/java/com/redpup/bracketbuster/sim/CalculationsTest.java
@@ -2,15 +2,85 @@ package com.redpup.bracketbuster.sim;
 
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.redpup.bracketbuster.model.Matchups.inverse;
 import static com.redpup.bracketbuster.util.AssertExt.assertThrows;
 
 import com.google.common.collect.Range;
+import com.redpup.bracketbuster.model.Lineup;
+import com.redpup.bracketbuster.model.MatchupMatrix;
+import com.redpup.bracketbuster.model.proto.MatchupMessage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class CalculationsTest {
+
+  private static final MatchupMessage MATCHUP_MESSAGE_A_1
+      = MatchupMessage.newBuilder()
+      .setPlayer("A")
+      .setOpponent("1")
+      .setWins(1)
+      .setGames(2)
+      .build();
+  private static final MatchupMessage MATCHUP_MESSAGE_A_2
+      = MatchupMessage.newBuilder()
+      .setPlayer("A")
+      .setOpponent("2")
+      .setWins(2)
+      .setGames(3)
+      .build();
+  private static final MatchupMessage MATCHUP_MESSAGE_A_3
+      = MatchupMessage.newBuilder()
+      .setPlayer("A")
+      .setOpponent("3")
+      .setWins(1)
+      .setGames(3)
+      .build();
+
+  private static final MatchupMessage MATCHUP_MESSAGE_B_1
+      = MatchupMessage.newBuilder()
+      .setPlayer("B")
+      .setOpponent("1")
+      .setWins(6)
+      .setGames(10)
+      .build();
+  private static final MatchupMessage MATCHUP_MESSAGE_B_2
+      = MatchupMessage.newBuilder()
+      .setPlayer("B")
+      .setOpponent("2")
+      .setWins(7)
+      .setGames(13)
+      .build();
+  private static final MatchupMessage MATCHUP_MESSAGE_B_3
+      = MatchupMessage.newBuilder()
+      .setPlayer("B")
+      .setOpponent("3")
+      .setWins(5)
+      .setGames(6)
+      .build();
+
+  private static final MatchupMessage MATCHUP_MESSAGE_C_1
+      = MatchupMessage.newBuilder()
+      .setPlayer("C")
+      .setOpponent("1")
+      .setWins(2)
+      .setGames(10)
+      .build();
+  private static final MatchupMessage MATCHUP_MESSAGE_C_2
+      = MatchupMessage.newBuilder()
+      .setPlayer("C")
+      .setOpponent("2")
+      .setWins(12)
+      .setGames(13)
+      .build();
+  private static final MatchupMessage MATCHUP_MESSAGE_C_3
+      = MatchupMessage.newBuilder()
+      .setPlayer("C")
+      .setOpponent("3")
+      .setWins(3)
+      .setGames(6)
+      .build();
 
   @Test
   public void maxIndex() {
@@ -176,6 +246,33 @@ public final class CalculationsTest {
             {0.7, 0.8, 0.9}},
         1, 2
     )).isEqualTo(new double[]{0.1, 0.2, 0.7, 0.8});
+  }
+
+  @Test
+  public void winRateBestTwoOfThreeOneBan_oneOpponent() {
+    MatchupMatrix matrix = MatchupMatrix.from(
+        MATCHUP_MESSAGE_A_1,
+        MATCHUP_MESSAGE_A_2,
+        MATCHUP_MESSAGE_A_3,
+        MATCHUP_MESSAGE_B_1,
+        MATCHUP_MESSAGE_B_2,
+        MATCHUP_MESSAGE_B_3,
+        MATCHUP_MESSAGE_C_1,
+        MATCHUP_MESSAGE_C_2,
+        MATCHUP_MESSAGE_C_3);
+
+    Lineup player = Lineup.ofDeckNames(matrix, "A", "B", "C");
+    Lineup opponent = Lineup.ofDeckNames(matrix, "1", "2", "3");
+
+    double winRate = Calculations.winRateBestTwoOfThreeOneBan(
+        player, opponent, matrix);
+
+    assertThat(winRate).isIn(Range.open(0.0, 1.0));
+
+    assertThat(player.metadata().getPlayedAgainst()).asList()
+        .containsExactly(1, 1, 1, 0, 0, 0).inOrder();
+    assertThat(player.metadata().getBanned()).asList()
+        .containsExactly(1, 0, 0, 0, 0, 0).inOrder();
   }
 
 }
