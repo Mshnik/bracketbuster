4b61998ba96428f08b4a501c1431edc34327586f
Mshnik <Mgpshnik@gmail.com>
2022-10-29 17:32:33 -0400

    Add WeightedDoubleMetric to track LineupMetadata metrics.

---
+++ src/main/java/com/redpup/bracketbuster/util/WeightedDoubleMetric.java
@@ -0,0 +1,167 @@
+package com.redpup.bracketbuster.util;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * A tracker on a stream of weighted doubles, to compute various statistics on it.
+ */
+public final class WeightedDoubleMetric {
+
+  private final double unweightedMean;
+  private final double weightedMean;
+  private final double stdDev;
+  private final double median;
+
+  private WeightedDoubleMetric(double unweightedMean, double weightedMean, double stdDev,
+      double median) {
+    this.unweightedMean = unweightedMean;
+    this.weightedMean = weightedMean;
+    this.stdDev = stdDev;
+    this.median = median;
+  }
+
+  /**
+   * Returns the unweighted mean of the metric.
+   */
+  public double getUnweightedMean() {
+    return unweightedMean;
+  }
+
+  /**
+   * Returns the weighted mean of the metric.
+   */
+  public double getWeightedMean() {
+    return weightedMean;
+  }
+
+  /**
+   * Returns the (unweighted) standard deviation of the metric.
+   */
+  public double getStdDev() {
+    return stdDev;
+  }
+
+  /**
+   * Returns the (unweighted) median of the metric.
+   */
+  public double getMedian() {
+    return median;
+  }
+
+  /**
+   * Returns a new {@link Builder}.
+   */
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  /**
+   * A builder for {@link WeightedDoubleMetric}.
+   */
+  public static final class Builder {
+
+    private double totalUnweightedValue;
+    private double totalUnweightedValueSquared;
+
+    private double totalWeightedValue;
+    private double totalWeight;
+    private int count;
+
+    private final Queue<Double> minHeap;
+    private final Queue<Double> maxHeap;
+
+    private Builder() {
+      minHeap = new PriorityQueue<>();
+      maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    }
+
+    /**
+     * Adds the given {@code value} to this builder. If all values are added through this method
+     * (instead of any through {@link #add(double, double)}), then all statistics are unweighted.
+     */
+    public Builder add(double value) {
+      return add(value, 1);
+    }
+
+    /**
+     * Adds the given {@code unweightedValue} with {@code weight} to this builder.
+     */
+    @CanIgnoreReturnValue
+    public Builder add(double unweightedValue, double weight) {
+      checkArgument(weight > 0);
+
+      totalUnweightedValue += unweightedValue;
+      totalUnweightedValueSquared += unweightedValue * unweightedValue;
+
+      totalWeightedValue += unweightedValue * weight;
+      totalWeight += weight;
+
+      count++;
+
+      if (minHeap.size() == maxHeap.size()) {
+        maxHeap.offer(unweightedValue);
+        minHeap.offer(maxHeap.poll());
+      } else {
+        minHeap.offer(unweightedValue);
+        maxHeap.offer(minHeap.poll());
+      }
+
+      return this;
+    }
+
+    /**
+     * Returns the unweighted mean of the values currently represented by this metric. (All weights
+     * are ignored).
+     */
+    private double getUnweightedMean() {
+      return totalUnweightedValue / count;
+    }
+
+    /**
+     * Returns the weighted mean of the values currently represented by this metric.
+     */
+    private double getWeightedMean() {
+      return totalWeightedValue / totalWeight;
+    }
+
+    /**
+     * Returns the standard deviation of the values currently represented by this metric.
+     */
+    private double getStdDev() {
+      double mean = getUnweightedMean();
+      return Math.sqrt(totalUnweightedValueSquared / count - (mean * mean));
+    }
+
+    /**
+     * Returns the median of the values currently represented by this metric.
+     */
+    private double getMedian() {
+      double median;
+      if (minHeap.size() > maxHeap.size()) {
+        median = minHeap.peek();
+      } else {
+        median = (checkNotNull(minHeap.peek()) + checkNotNull(maxHeap.peek())) / 2.0;
+      }
+      return median;
+    }
+
+    /**
+     * Builds this into a {@link WeightedDoubleMetric}.
+     */
+    public WeightedDoubleMetric build() {
+      if (count == 0) {
+        return new WeightedDoubleMetric(0, 0, 0, 0);
+      } else {
+        return new WeightedDoubleMetric(getUnweightedMean(), getWeightedMean(), getStdDev(),
+            getMedian());
+      }
+    }
+  }
+
+}
---
+++ src/test/java/com/redpup/bracketbuster/util/WeightedDoubleMetricTest.java
@@ -0,0 +1,131 @@
+package com.redpup.bracketbuster.util;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class WeightedDoubleMetricTest {
+
+  private static final double ERROR = 1.0e-8;
+
+  @Test
+  public void empty() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().build();
+    assertThat(metric.getUnweightedMean()).isEqualTo(0.0);
+    assertThat(metric.getWeightedMean()).isEqualTo(0.0);
+    assertThat(metric.getStdDev()).isEqualTo(0.0);
+    assertThat(metric.getMedian()).isEqualTo(0.0);
+  }
+
+  @Test
+  public void oneValue_unweighted() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5).build();
+    assertThat(metric.getWeightedMean()).isEqualTo(2.5);
+    assertThat(metric.getUnweightedMean()).isEqualTo(2.5);
+    assertThat(metric.getStdDev()).isEqualTo(0.0);
+    assertThat(metric.getMedian()).isEqualTo(2.5);
+  }
+
+  @Test
+  public void oneValue_weighted() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5, 0.5).build();
+    assertThat(metric.getWeightedMean()).isEqualTo(2.5);
+    assertThat(metric.getUnweightedMean()).isEqualTo(2.5);
+    assertThat(metric.getStdDev()).isEqualTo(0.0);
+    assertThat(metric.getMedian()).isEqualTo(2.5);
+  }
+
+  @Test
+  public void twoValues_same_unweighted() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5).add(2.5).build();
+    assertThat(metric.getWeightedMean()).isEqualTo(2.5);
+    assertThat(metric.getUnweightedMean()).isEqualTo(2.5);
+    assertThat(metric.getStdDev()).isEqualTo(0.0);
+    assertThat(metric.getMedian()).isEqualTo(2.5);
+  }
+
+  @Test
+  public void twoValues_same_weighted_sameWeights() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5, 0.5).add(2.5, 0.5)
+        .build();
+    assertThat(metric.getUnweightedMean()).isEqualTo(2.5);
+    assertThat(metric.getWeightedMean()).isEqualTo(2.5);
+    assertThat(metric.getStdDev()).isEqualTo(0.0);
+    assertThat(metric.getMedian()).isEqualTo(2.5);
+  }
+
+  @Test
+  public void twoValues_same_weighted_differentWeights() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5, 0.75).add(2.5, 0.5)
+        .build();
+    assertThat(metric.getUnweightedMean()).isEqualTo(2.5);
+    assertThat(metric.getWeightedMean()).isEqualTo(2.5);
+    assertThat(metric.getStdDev()).isEqualTo(0.0);
+    assertThat(metric.getMedian()).isEqualTo(2.5);
+  }
+
+  @Test
+  public void twoValues_different_unweighted() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5).add(3.5).build();
+    assertThat(metric.getWeightedMean()).isEqualTo(3.0);
+    assertThat(metric.getUnweightedMean()).isEqualTo(3.0);
+    assertThat(metric.getStdDev()).isEqualTo(0.5);
+    assertThat(metric.getMedian()).isEqualTo(3.0);
+  }
+
+  @Test
+  public void twoValues_different_weighted_sameWeights() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5, 0.5).add(3.5, 0.5)
+        .build();
+    assertThat(metric.getUnweightedMean()).isEqualTo(3.0);
+    assertThat(metric.getWeightedMean()).isEqualTo(3.0);
+    assertThat(metric.getStdDev()).isEqualTo(0.5);
+    assertThat(metric.getMedian()).isEqualTo(3.0);
+  }
+
+  @Test
+  public void twoValues_different_weighted_differentWeights() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5, 0.75).add(3.5, 0.5)
+        .build();
+    assertThat(metric.getUnweightedMean()).isEqualTo(3.0);
+    assertThat(metric.getWeightedMean()).isEqualTo(2.9);
+    assertThat(metric.getStdDev()).isEqualTo(0.5);
+    assertThat(metric.getMedian()).isEqualTo(3.0);
+  }
+
+
+  @Test
+  public void threeValues_different_unweighted() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5).add(3.5).add(0.0).build();
+    assertThat(metric.getWeightedMean()).isEqualTo(2.0);
+    assertThat(metric.getUnweightedMean()).isEqualTo(2.0);
+    assertThat(metric.getStdDev()).isWithin(ERROR).of(1.4719601443);
+    assertThat(metric.getMedian()).isEqualTo(2.5);
+  }
+
+  @Test
+  public void threeValues_different_weighted_sameWeights() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5, 0.5).add(3.5, 0.5)
+        .add(0.0, 0.5)
+        .build();
+    assertThat(metric.getUnweightedMean()).isEqualTo(2.0);
+    assertThat(metric.getWeightedMean()).isEqualTo(2.0);
+    assertThat(metric.getStdDev()).isWithin(ERROR).of(1.4719601443);
+    assertThat(metric.getMedian()).isEqualTo(2.5);
+  }
+
+  @Test
+  public void threeValues_different_weighted_differentWeights() {
+    WeightedDoubleMetric metric = WeightedDoubleMetric.builder().add(2.5, 0.75).add(3.5, 0.5)
+        .add(0.0, 0.15)
+        .build();
+    assertThat(metric.getUnweightedMean()).isEqualTo(2.0);
+    assertThat(metric.getWeightedMean()).isWithin(ERROR).of(2.5892857142);
+    assertThat(metric.getStdDev()).isWithin(ERROR).of(1.4719601443);
+    assertThat(metric.getMedian()).isEqualTo(2.5);
+  }
+
+}
